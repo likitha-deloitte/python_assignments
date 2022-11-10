822401f99edde7176cf1ea382c13413cda5b3e84
mulikitha <mulikitha@deloitte.com>
2022-11-10 23:06:52 +0530

    "list of pairs in a string"

---
+++ oops.py
@@ -0,0 +1,32 @@
+class StringClass:
+    def __init__(self):
+        self.strings = self
+
+    def stringlength(self):
+        print(len(self.strings));
+
+    def converttolist(self):
+        listdata = []
+        listdata[:0] = self.strings
+        return listdata;
+
+class PairsPossible(StringClass):
+    def __init__(self):
+        self.strings = input("Enter string: ")
+
+    def getcombinations(self):
+        from itertools import combinations
+        list1 = []
+        list1[:0] = self.strings
+        res = list(combinations(list1, 2))
+        return res
+    def printlist(self,lists):
+        print(lists);
+
+x = PairsPossible()
+x.stringlength()
+x.converttolist()
+listes = x.getcombinations()
+x.printlist(listes);
+
+
