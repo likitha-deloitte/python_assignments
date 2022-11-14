f74d443927fa1efd2f88deeed06e1a3d3eb8ee4f
mulikitha <mulikitha@deloitte.com>
2022-11-14 23:09:16 +0530

    "Pascal Triangle "

--- pascaltriangle.py
+++ pascaltriangle.py
@@ -3,7 +3,7 @@ list = []
 for i in range(N):
     list1 = []
     for j in range(i+1):
-        if j==0 or j==i:
+        if j == 0 or j == i:
            list1.append(1)
         else:
            list1.append(list[i-1][j-1] + list[i-1][j])
@@ -11,7 +11,7 @@ for i in range(N):
 
 for i in range(N):
     for j in range(N-1):
-        print(" ",end=" ")
+        print(" ", end=" ")
     for j in range(i + 1):
         print(list[i][j], end=" ")
     print()
